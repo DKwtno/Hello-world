44fed08e7ddc88c2d63a5fd90f92f1df9d6c74a4
DKwtno <wei609778992@gmail.com>
2017-09-29 13:35:12 +0800

    print
    print the item of list L ranks int p

---
+++ docs/printLots.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author DKwtno
+ * print the item of L ranked p
+ */
+public class PrintLots {
+	public static void printLots(int[] L, int p){
+		System.out.println(L[p-1]);
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] list={1,2,3,4};
+		int p=2;
+		printLots(list,p);
+		
+	}
+	
+}
+
+	
