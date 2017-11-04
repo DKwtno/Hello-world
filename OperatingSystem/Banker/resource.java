be5cd4525c59025faf80e98c4d095c28452c1561
DKwtno <wei609778992@gmail.com>
2017-11-04 09:04:36 +0800

    Resource
    资源类

---
+++ OperatingSystem/Banker/resource.java
@@ -0,0 +1,23 @@
+
+public class resource {
+	private int num;
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+		rsor=new int[num];
+	}
+
+	private int[] rsor;
+
+	public int[] getRsor() {
+		return rsor;
+	}
+
+	public void setRsor(int[] rsor) {
+		this.rsor = rsor;
+	}
+	
+}
